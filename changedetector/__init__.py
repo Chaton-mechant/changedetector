8895530f262f3a51d724cd8ba09a36c6751a0cef
Delphin Blehoussi <delphin.blehoussi@student.uclouvain.be>
2022-06-18 13:12:00 +0200

    __init__.py

--- changedetector/__init__.py
+++ changedetector/__init__.py
@@ -13,7 +13,5 @@ __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2022 LuxLuth'
 __version__ = '0.0.1'
 
-# The directory where the package is imported in.
-__path__ = __import__('pkgutil').extend_path(__path__, __name__)
 
 from .detectchange import *
